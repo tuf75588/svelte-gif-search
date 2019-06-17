4a299513537e6de7804d1472978a3b1301e2af13
Andrew Davis <atd285@gmail.com>
2019-06-16 21:36:25 -0400

    basic logic for app done

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  let name = "";
+  let loading = false;
+  const apiKey = "m7NZGH33sHzM9WLEheMewhyoRSrjEsGN";
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}`;
+  let gifs = [];
+  function formSubmitted(event) {
+    event.preventDefault();
+
+    return fetch(`${url}&q=${name}`)
+      .then(data => {
+        loading = true;
+        return data.json();
+      })
+
+      .then(({ data }) => {
+        gifs = data;
+        loading = false;
+      });
+  }
+</script>
+
+<style>
+
+</style>
+
+<form on:submit={formSubmitted}>
+  <label for="search">Search</label>
+  <input bind:value={name} type="text" placeholder="search term" />
+  <button type="submit">Submit</button>
+</form>
+<p>{name || 'Andrew'} </p>
+{#if loading}
+  <h1>loading...</h1>
+{/if}
+<div class="container" />
